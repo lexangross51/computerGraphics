047fbad2620bf69cd08a33dc18ca63ccfa7eb668
lexangross51 <lexangross51@gmail.com>
2022-09-09 16:48:53 +0700

    Структура точки

---
+++ cg_1/cg_1/src/Point2D.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cg_1.src
+{
+    public struct Point2D
+    {
+        short X { get; set; }
+        short Y { get; set; }
+
+        public Point2D(short x, short y) => (X, Y) = (x, y);
+    }
+}
