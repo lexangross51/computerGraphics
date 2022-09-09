ac390841ff95c9ccefd681f380f04ca9401e3771
lexangross51 <lexangross51@gmail.com>
2022-09-09 16:49:02 +0700

    Структура примитива

---
+++ cg_1/cg_1/src/Primitive.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SharpGL;
+
+namespace cg_1.src
+{
+    public class StripLine
+    {
+        public List<Point2D> Points { get; set; }
+        public Color Color { get; set; }
+
+        public StripLine()
+        {
+            Points = new List<Point2D>();
+            Color = new Color();
+        }
+    }
+
+
+
+    public struct Color
+    {
+        byte R { get; set; }
+        byte G { get; set; }
+        byte B { get; set; }
+
+        public Color(byte r = 0, byte g = 0, byte b = 0) 
+            => (R, G, B) = (r, g, b);
+    }
+}
