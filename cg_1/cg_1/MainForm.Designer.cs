aafe315fd55696ff35e25ef5b13fdd11ab8bb6c0
lexangross51 <lexangross51@gmail.com>
2022-09-09 21:18:02 +0700

    Добавлена логика работы с наборами примитивов

--- cg_1/cg_1/MainForm.Designer.cs
+++ cg_1/cg_1/MainForm.Designer.cs
@@ -34,8 +34,18 @@
             this.statusXPosValue = new System.Windows.Forms.ToolStripStatusLabel();
             this.statusYPosName = new System.Windows.Forms.ToolStripStatusLabel();
             this.statusYPosValue = new System.Windows.Forms.ToolStripStatusLabel();
+            this.ChangeSet = new System.Windows.Forms.NumericUpDown();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.ChangePrimitive = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.DeleteSet = new System.Windows.Forms.Button();
+            this.DeletePrimitive = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.GL)).BeginInit();
             this.statusBar.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ChangeSet)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ChangePrimitive)).BeginInit();
             this.SuspendLayout();
             // 
             // GL
@@ -44,13 +54,13 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.GL.DrawFPS = false;
-            this.GL.Location = new System.Drawing.Point(13, 2);
-            this.GL.Margin = new System.Windows.Forms.Padding(4);
+            this.GL.Location = new System.Drawing.Point(0, 2);
+            this.GL.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.GL.Name = "GL";
             this.GL.OpenGLVersion = SharpGL.Version.OpenGLVersion.OpenGL2_1;
             this.GL.RenderContextType = SharpGL.RenderContextType.DIBSection;
             this.GL.RenderTrigger = SharpGL.RenderTrigger.TimerBased;
-            this.GL.Size = new System.Drawing.Size(920, 644);
+            this.GL.Size = new System.Drawing.Size(933, 644);
             this.GL.TabIndex = 0;
             this.GL.OpenGLInitialized += new System.EventHandler(this.GL_OpenGLInitialized);
             this.GL.OpenGLDraw += new SharpGL.RenderEventHandler(this.GL_OpenGLDraw);
@@ -76,7 +86,7 @@
             // statusXPosName
             // 
             this.statusXPosName.Name = "statusXPosName";
-            this.statusXPosName.Size = new System.Drawing.Size(21, 20);
+            this.statusXPosName.Size = new System.Drawing.Size(21, 24);
             this.statusXPosName.Text = "X:";
             // 
             // statusXPosValue
@@ -91,7 +101,7 @@
             // statusYPosName
             // 
             this.statusYPosName.Name = "statusYPosName";
-            this.statusYPosName.Size = new System.Drawing.Size(20, 20);
+            this.statusYPosName.Size = new System.Drawing.Size(20, 24);
             this.statusYPosName.Text = "Y:";
             // 
             // statusYPosValue
@@ -103,11 +113,83 @@
             this.statusYPosValue.Size = new System.Drawing.Size(45, 24);
             this.statusYPosValue.Text = "0000";
             // 
+            // ChangeSet
+            // 
+            this.ChangeSet.Location = new System.Drawing.Point(143, 17);
+            this.ChangeSet.Name = "ChangeSet";
+            this.ChangeSet.ReadOnly = true;
+            this.ChangeSet.Size = new System.Drawing.Size(84, 22);
+            this.ChangeSet.TabIndex = 2;
+            this.ChangeSet.ValueChanged += new System.EventHandler(this.ChangeSet_ValueChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.DeletePrimitive);
+            this.groupBox1.Controls.Add(this.DeleteSet);
+            this.groupBox1.Controls.Add(this.ChangePrimitive);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.ChangeSet);
+            this.groupBox1.Location = new System.Drawing.Point(952, 2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(345, 85);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Режим набора";
+            // 
+            // ChangePrimitive
+            // 
+            this.ChangePrimitive.Location = new System.Drawing.Point(143, 49);
+            this.ChangePrimitive.Name = "ChangePrimitive";
+            this.ChangePrimitive.ReadOnly = true;
+            this.ChangePrimitive.Size = new System.Drawing.Size(84, 22);
+            this.ChangePrimitive.TabIndex = 5;
+            this.ChangePrimitive.ValueChanged += new System.EventHandler(this.ChangePrimitive_ValueChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(131, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Текущий примитив";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Текущий набор";
+            // 
+            // DeleteSet
+            // 
+            this.DeleteSet.Location = new System.Drawing.Point(243, 16);
+            this.DeleteSet.Name = "DeleteSet";
+            this.DeleteSet.Size = new System.Drawing.Size(75, 23);
+            this.DeleteSet.TabIndex = 6;
+            this.DeleteSet.Text = "Удалить";
+            this.DeleteSet.UseVisualStyleBackColor = true;
+            this.DeleteSet.Click += new System.EventHandler(this.DeleteSet_Click);
+            // 
+            // DeletePrimitive
+            // 
+            this.DeletePrimitive.Location = new System.Drawing.Point(243, 49);
+            this.DeletePrimitive.Name = "DeletePrimitive";
+            this.DeletePrimitive.Size = new System.Drawing.Size(75, 23);
+            this.DeletePrimitive.TabIndex = 7;
+            this.DeletePrimitive.Text = "Удалить";
+            this.DeletePrimitive.UseVisualStyleBackColor = true;
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1309, 680);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.statusBar);
             this.Controls.Add(this.GL);
             this.Name = "MainForm";
@@ -115,6 +197,10 @@
             ((System.ComponentModel.ISupportInitialize)(this.GL)).EndInit();
             this.statusBar.ResumeLayout(false);
             this.statusBar.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ChangeSet)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ChangePrimitive)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -128,6 +214,13 @@
         private System.Windows.Forms.ToolStripStatusLabel statusXPosValue;
         private System.Windows.Forms.ToolStripStatusLabel statusYPosName;
         private System.Windows.Forms.ToolStripStatusLabel statusYPosValue;
+        private System.Windows.Forms.NumericUpDown ChangeSet;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.NumericUpDown ChangePrimitive;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button DeleteSet;
+        private System.Windows.Forms.Button DeletePrimitive;
     }
 }
 
--- cg_1/cg_1/MainForm.cs
+++ cg_1/cg_1/MainForm.cs
@@ -10,6 +10,9 @@ namespace ComputerGraphics
     {
         private List<StripLine> _lines = new List<StripLine> ();
         private readonly StripLine _line = new StripLine();
+        private byte _currentSet = 0;
+        private bool _isDdrawingCurrent = true;
+
 
         public MainForm()
         {
@@ -31,17 +34,37 @@ namespace ComputerGraphics
             gl.LoadIdentity();
         }
 
+        private void GL_Resized(object sender, EventArgs e)
+        {
+            GL_OpenGLInitialized(sender, e);
+        }
+
         private void GL_OpenGLDraw(object sender, RenderEventArgs args)
         {
             OpenGL gl = GL.OpenGL;
 
             gl.Clear(OpenGL.GL_COLOR_BUFFER_BIT);
-            gl.Color(0, 0, 0);
-
 
 
             for (int i = 0; i < _lines.Count; i++)
             {
+                gl.Color(_lines[i].Color.R, _lines[i].Color.G, _lines[i].Color.B);
+
+                if (_lines[i].SetNumber == _currentSet)
+                {
+                    gl.PointSize(10);
+                    gl.Enable(OpenGL.GL_POINT_SMOOTH);
+                    gl.Begin(OpenGL.GL_POINTS);
+
+                    foreach (var p in _lines[i].Points)
+                    {
+                        gl.Vertex(p.X, p.Y);
+                    }
+
+                    gl.End();
+                }
+
+                gl.LineWidth(_lines[i].Thickness);
                 gl.Begin(OpenGL.GL_LINE_STRIP);
 
                 for (int j = 0; j < _lines[i].Points.Count; j++)
@@ -52,28 +75,38 @@ namespace ComputerGraphics
                 gl.End();
             }
 
-
-
-            gl.Begin(OpenGL.GL_LINE_STRIP);
-            foreach (var p in _line.Points)
+            if (_isDdrawingCurrent)
             {
-                gl.Vertex(p.X, p.Y);
+                gl.PointSize(10);
+                gl.Color(_line.Color.R, _line.Color.G, _line.Color.B);
+                gl.Enable(OpenGL.GL_POINT_SMOOTH);
+                gl.Begin(OpenGL.GL_POINTS);
+
+                foreach (var p in _line.Points)
+                {
+                    gl.Vertex(p.X, p.Y);
+                }
+
+                gl.End();
+                gl.Disable(OpenGL.GL_POINT_SMOOTH);
+
+                gl.Begin(OpenGL.GL_LINE_STRIP);
+
+                foreach (var p in _line.Points)
+                {
+                    gl.Vertex(p.X, p.Y);
+                }
+
+                gl.End();
             }
-            gl.End();
-
-
 
             gl.Finish();
-
-        }
-
-        private void GL_Resized(object sender, EventArgs e)
-        {
-            GL_OpenGLInitialized(sender, e);
         }
 
         private void GL_MouseClick(object sender, MouseEventArgs e)
         {
+            _isDdrawingCurrent = true;
+
             short mouseX = (short)e.X;
             short mouseY = (short)(GL.Height - (short)e.Y);
 
@@ -82,8 +115,18 @@ namespace ComputerGraphics
 
         private void GL_KeyDown(object sender, KeyEventArgs e)
         {
-            _lines.Add(_line.Clone() as StripLine);
-            _line.Points.Clear();
+            if (e.KeyCode == Keys.Space)
+            {
+                _lines.Add(_line.Clone() as StripLine);
+                _line.Points.Clear();
+                _isDdrawingCurrent = false;
+            }
+            if (e.KeyCode == Keys.Z)
+            {
+                _line.SetNumber = ++_currentSet;
+                ChangeSet.Maximum = _currentSet;
+                ChangeSet.Value++;
+            }
         }
 
         private void GL_MouseMove(object sender, MouseEventArgs e)
@@ -93,6 +136,37 @@ namespace ComputerGraphics
 
             statusXPosValue.Text = xPos.ToString();
             statusYPosValue.Text = yPos.ToString();
+        }
+
+
+        // Панель управление
+        private void ChangeSet_ValueChanged(object sender, EventArgs e)
+        {
+            _currentSet = (byte)ChangeSet.Value;
+        }
+
+        private void ChangePrimitive_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void DeleteSet_Click(object sender, EventArgs e)
+        {
+            _currentSet = (byte)ChangeSet.Value;
+
+            for (int i = 0; i < _lines.Count; i++)
+            {
+                if (_lines[i].SetNumber == _currentSet)
+                {
+                    _lines.RemoveAt(i);
+                    i = i == 0 ? 0 : i--;
+                }
+                
+                if (_lines[i].SetNumber > _currentSet)
+                {
+                    _lines[i].SetNumber--;
+                }
+            }
         }
     }
 }
