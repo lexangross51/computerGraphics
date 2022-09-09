8932606728b39552f510935ba7f2bfa09451ba0f
lexangross51 <lexangross51@gmail.com>
2022-09-09 16:49:32 +0700

    Главная форма

---
+++ cg_1/cg_1/MainForm.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace cg_1
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.GL = new SharpGL.OpenGLControl();
+            ((System.ComponentModel.ISupportInitialize)(this.GL)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // GL
+            // 
+            this.GL.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.GL.DrawFPS = false;
+            this.GL.Location = new System.Drawing.Point(13, 13);
+            this.GL.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.GL.Name = "GL";
+            this.GL.OpenGLVersion = SharpGL.Version.OpenGLVersion.OpenGL2_1;
+            this.GL.RenderContextType = SharpGL.RenderContextType.DIBSection;
+            this.GL.RenderTrigger = SharpGL.RenderTrigger.TimerBased;
+            this.GL.Size = new System.Drawing.Size(979, 726);
+            this.GL.TabIndex = 0;
+            this.GL.OpenGLInitialized += new System.EventHandler(this.GL_OpenGLInitialized);
+            this.GL.OpenGLDraw += new SharpGL.RenderEventHandler(this.GL_OpenGLDraw);
+            this.GL.Resized += new System.EventHandler(this.GL_Resized);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1381, 752);
+            this.Controls.Add(this.GL);
+            this.Name = "MainForm";
+            this.Text = "Отрисовка примитивов";
+            ((System.ComponentModel.ISupportInitialize)(this.GL)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private SharpGL.OpenGLControl GL;
+    }
+}
+
---
+++ cg_1/cg_1/MainForm.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SharpGL;
+
+namespace cg_1
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void GL_OpenGLInitialized(object sender, EventArgs e)
+        {
+            OpenGL gl = GL.OpenGL;
+
+            gl.Disable(OpenGL.GL_DEPTH_TEST);
+            gl.ClearColor(1f, 1f, 1f, 1f);
+            gl.Clear(OpenGL.GL_COLOR_BUFFER_BIT);
+
+            gl.MatrixMode(OpenGL.GL_PROJECTION);
+            gl.LoadIdentity();
+            gl.Ortho2D(0, GL.Width, 0, GL.Height);
+            gl.MatrixMode(OpenGL.GL_MODELVIEW);
+            gl.LoadIdentity();
+        }
+
+        private void GL_OpenGLDraw(object sender, SharpGL.RenderEventArgs args)
+        {
+
+        }
+
+        private void GL_Resized(object sender, EventArgs e)
+        {
+            GL_OpenGLInitialized(sender, e);
+        }
+    }
+}
