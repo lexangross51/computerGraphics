b61f4fcab4295c13775fd198cf5c957493d62e47
lexangross51 <lexangross51@gmail.com>
2022-09-12 20:17:58 +0700

    Расширен класс List путем добавления метода IsEmpty

---
+++ cg_1/cg_1/Source/Extensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputerGraphics.Source
+{
+    public static class ListExtensinos
+    {
+        public static bool IsEmpty<T>(this List<T> list)
+        {
+            return list.Count == 0;
+        }
+    }
+}
