58cf63234b83768a80a9cd2801d9f0701ffd40ab
lexangross51 <lexangross51@gmail.com>
2022-09-12 20:19:08 +0700

    Удалил номер набора. Поправил клонирование. Удалил структуру Color, вместо которой была использована системная

--- cg_1/cg_1/Source/Primitive.cs
+++ cg_1/cg_1/Source/Primitive.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Drawing;
 using System.Collections.Generic;
 
 namespace ComputerGraphics.Source
@@ -8,30 +9,25 @@ namespace ComputerGraphics.Source
         public List<Point2D> Points { get; set; }
         public Color Color { get; set; }
         public float Thickness { get; set; }
-        public byte Set { get; set; }
 
         public StripLine()
         {
             Points = new List<Point2D>();
             Color = new Color();
             Thickness = 1.0f;
-            Set = 0;
         }
 
         public object Clone() => new StripLine {
             Points = new List<Point2D>(Points),
             Color = Color,
-            Set = Set
+            Thickness = Thickness
         };
-    }
 
-    public struct Color
-    {
-        public byte R { get; set; }
-        public byte G { get; set; }
-        public byte B { get; set; }
-
-        public Color(byte r = 0, byte g = 0, byte b = 0) 
-            => (R, G, B) = (r, g, b);
+        public void Reset()
+        {
+            Points.Clear();
+            Color = new Color();
+            Thickness = 1.0f;
+        }
     }
 }
