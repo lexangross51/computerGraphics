932a5fa9a669e813f4317c1a824eb39f180d4461
lexangross51 <lexangross51@gmail.com>
2022-10-01 16:57:41 +0700

    Добавлены методы для масштабирования и поворота примитива

--- cg_1/cg_1/Source/Primitive.cs
+++ cg_1/cg_1/Source/Primitive.cs
@@ -10,6 +10,24 @@ namespace ComputerGraphics.Source
         public Color Color { get; set; }
         public float Thickness { get; set; }
         public ushort Stipple { get; set; }
+        public float ScaleXY { get; private set; }
+        public float Angle { get; private set; }
+
+        public Point2D MassCenter()
+        {
+            float x = 0, y = 0;
+
+            foreach (var p in Points)
+            {
+                x += p.X;
+                y += p.Y;
+            }
+
+            x = x / Points.Count;
+            y = y / Points.Count;
+
+            return new Point2D(x, y);
+        }
 
         public StripLine()
         {
@@ -17,6 +35,7 @@ namespace ComputerGraphics.Source
             Color = new Color();
             Thickness = 1.0f;
             Stipple = 0xFFFF;
+            ScaleXY = 1.0f;
         }
 
         public object Clone() => new StripLine
@@ -24,7 +43,8 @@ namespace ComputerGraphics.Source
             Points = new List<Point2D>(Points),
             Color = Color,
             Thickness = Thickness,
-            Stipple = Stipple
+            Stipple = Stipple,
+            ScaleXY = ScaleXY
         };
 
         public void Reset()
@@ -33,6 +53,44 @@ namespace ComputerGraphics.Source
             Color = new Color();
             Thickness = 1.0f;
             Stipple = 0xFFFF;
+            ScaleXY = 1.0f;
+        }
+
+        public void Scale(Point2D pivot, float scaling)
+        {
+            ScaleXY *= scaling;
+
+            float xStep = pivot.X * scaling - pivot.X;
+            float yStep = pivot.Y * scaling - pivot.Y;
+
+            for (int i = 0; i < Points.Count; i++)
+            {
+                var x = Points[i].X * scaling - xStep;
+                var y = Points[i].Y * scaling - yStep;
+
+                Points[i] = new Point2D(x, y);
+            }
+        }
+
+        public void Rotate(Point2D pivot, float angle)
+        {
+            Angle += angle;
+
+            float radAngle = (float)(Math.PI * angle / 180.0);
+
+            for (int i = 0; i < Points.Count; i++)
+            {
+                var x = Points[i].X - pivot.X;
+                var y = Points[i].Y - pivot.Y;
+
+                var newX = (float)(x * Math.Cos(radAngle) + y * Math.Sin(radAngle));
+                var newY = (float)(-x * Math.Sin(radAngle) + y * Math.Cos(radAngle));
+
+                newX += pivot.X;
+                newY += pivot.Y;
+
+                Points[i] = new Point2D(newX, newY);
+            }
         }
     }
 }
