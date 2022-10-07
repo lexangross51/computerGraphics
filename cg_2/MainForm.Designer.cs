1efb18a79a2d32d4ef98a418df704202a94c6341
lexangross51 <lexangross51@gmail.com>
2022-10-07 21:51:31 +0700

    Попытка сделать вращение камеры

--- cg_2/MainForm.Designer.cs
+++ cg_2/MainForm.Designer.cs
@@ -30,11 +30,18 @@
         {
             this.GL = new SharpGL.OpenGLControl();
             this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.Scene = new System.Windows.Forms.GroupBox();
+            this.ProjectionModeLabel = new System.Windows.Forms.Label();
+            this.ProjectionModeList = new System.Windows.Forms.ComboBox();
             ((System.ComponentModel.ISupportInitialize)(this.GL)).BeginInit();
+            this.Scene.SuspendLayout();
             this.SuspendLayout();
             // 
             // GL
             // 
+            this.GL.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.GL.DrawFPS = false;
             this.GL.Location = new System.Drawing.Point(0, 7);
             this.GL.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
@@ -42,11 +49,13 @@
             this.GL.OpenGLVersion = SharpGL.Version.OpenGLVersion.OpenGL2_1;
             this.GL.RenderContextType = SharpGL.RenderContextType.DIBSection;
             this.GL.RenderTrigger = SharpGL.RenderTrigger.TimerBased;
-            this.GL.Size = new System.Drawing.Size(1242, 724);
+            this.GL.Size = new System.Drawing.Size(1250, 724);
             this.GL.TabIndex = 0;
             this.GL.OpenGLInitialized += new System.EventHandler(this.GL_OpenGLInitialized);
             this.GL.OpenGLDraw += new SharpGL.RenderEventHandler(this.GL_OpenGLDraw);
             this.GL.Resized += new System.EventHandler(this.GL_Resized);
+            this.GL.MouseClick += new System.Windows.Forms.MouseEventHandler(this.GL_MouseClick);
+            this.GL.MouseMove += new System.Windows.Forms.MouseEventHandler(this.GL_MouseMove);
             // 
             // statusStrip1
             // 
@@ -57,16 +66,51 @@
             this.statusStrip1.TabIndex = 1;
             this.statusStrip1.Text = "statusStrip1";
             // 
+            // Scene
+            // 
+            this.Scene.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.Scene.Controls.Add(this.ProjectionModeLabel);
+            this.Scene.Controls.Add(this.ProjectionModeList);
+            this.Scene.Location = new System.Drawing.Point(1257, 7);
+            this.Scene.Name = "Scene";
+            this.Scene.Size = new System.Drawing.Size(252, 132);
+            this.Scene.TabIndex = 2;
+            this.Scene.TabStop = false;
+            this.Scene.Text = "Сцена";
+            // 
+            // ProjectionModeLabel
+            // 
+            this.ProjectionModeLabel.AutoSize = true;
+            this.ProjectionModeLabel.Location = new System.Drawing.Point(6, 23);
+            this.ProjectionModeLabel.Name = "ProjectionModeLabel";
+            this.ProjectionModeLabel.Size = new System.Drawing.Size(79, 20);
+            this.ProjectionModeLabel.TabIndex = 4;
+            this.ProjectionModeLabel.Text = "Проекция";
+            // 
+            // ProjectionModeList
+            // 
+            this.ProjectionModeList.FormattingEnabled = true;
+            this.ProjectionModeList.Items.AddRange(new object[] {
+            "Перспективная",
+            "Ортографическая"});
+            this.ProjectionModeList.Location = new System.Drawing.Point(91, 20);
+            this.ProjectionModeList.Name = "ProjectionModeList";
+            this.ProjectionModeList.Size = new System.Drawing.Size(151, 28);
+            this.ProjectionModeList.TabIndex = 3;
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1521, 758);
+            this.Controls.Add(this.Scene);
             this.Controls.Add(this.statusStrip1);
             this.Controls.Add(this.GL);
             this.Name = "MainForm";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.GL)).EndInit();
+            this.Scene.ResumeLayout(false);
+            this.Scene.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -76,5 +120,8 @@
 
         private SharpGL.OpenGLControl GL;
         private StatusStrip statusStrip1;
+        private GroupBox Scene;
+        private Label ProjectionModeLabel;
+        private ComboBox ProjectionModeList;
     }
 }
--- cg_2/MainForm.cs
+++ cg_2/MainForm.cs
@@ -1,19 +1,88 @@
 namespace cg_2;
+
+public enum ProjectionMode : byte
+{
+    Orthographic,
+    Perspestive
+}
+
+
 public partial class MainForm : Form
 {
-    public MainForm() => InitializeComponent();
+    Camera _mainCamera;
 
-    private void GL_OpenGLDraw(object sender, SharpGL.RenderEventArgs args)
+    public MainForm()
     {
-
+        InitializeComponent();
+        _mainCamera = new Camera();
     }
 
     private void GL_OpenGLInitialized(object sender, EventArgs e)
     {
+        var gl = GL.OpenGL;
 
+        gl.Enable(OpenGL.GL_DEPTH_TEST);
     }
 
     private void GL_Resized(object sender, EventArgs e)
+    {
+        var gl = GL.OpenGL;
+
+        gl.Viewport(0, 0, GL.Width, GL.Height);
+        gl.MatrixMode(OpenGL.GL_PROJECTION);
+        gl.LoadIdentity();
+        gl.Frustum(0, GL.Width, 0, GL.Height, -100, 100);
+        //gl.Perspective(70, GL.Width / GL.Height, -10, 100);
+        gl.MatrixMode(OpenGL.GL_MODELVIEW);
+        gl.LoadIdentity();
+    }
+
+    private void GL_OpenGLDraw(object sender, RenderEventArgs args)
+    {
+        var gl = GL.OpenGL;
+
+        gl.ClearColor(1, 1, 1, 1);
+        gl.Clear(OpenGL.GL_COLOR_BUFFER_BIT | OpenGL.GL_DEPTH_BUFFER_BIT);
+
+
+        gl.LoadIdentity();
+
+        var eye = _mainCamera.Position;
+        var view = _mainCamera.View;
+        var up = _mainCamera.Up;
+
+        gl.LookAt(eye.X, eye.Y, eye.Z, view.X, view.Y, view.Z, up.X, up.Y, up.Z);
+
+        #region Отрисовка сетки
+
+        gl.Color((byte)0, (byte)0, (byte)0);
+
+        for (float i = -1000; i < 1000; i += 0.1f)
+        {
+            gl.Begin(OpenGL.GL_LINES);
+            gl.Vertex(i, 0.0f, -1000.0f);
+            gl.Vertex(i, 0.0f, 1000.0f);
+            gl.Vertex(-1000.0f, 0.0f, i);
+            gl.Vertex(1000.0f, 0.0f, i);
+            gl.End();
+        }
+
+        gl.Finish();
+
+        #endregion
+
+
+    }
+
+    private void GL_MouseMove(object sender, MouseEventArgs e)
+    {
+        var xPos = (float)e.X;
+        var yPos = (float)e.Y;
+
+        _mainCamera.LookAt(xPos, yPos);
+    }
+
+    private void GL_MouseClick(object sender, MouseEventArgs e)
     {
 
     }
