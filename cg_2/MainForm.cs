cedbaa9366737707370eeec6e09c350b8140cf72
lexangross51 <lexangross51@gmail.com>
2022-10-07 11:23:01 +0700

    Перенес проект под .Net Core

---
+++ cg_2/MainForm.cs
@@ -0,0 +1,20 @@
+namespace cg_2;
+public partial class MainForm : Form
+{
+    public MainForm() => InitializeComponent();
+
+    private void GL_OpenGLDraw(object sender, SharpGL.RenderEventArgs args)
+    {
+
+    }
+
+    private void GL_OpenGLInitialized(object sender, EventArgs e)
+    {
+
+    }
+
+    private void GL_Resized(object sender, EventArgs e)
+    {
+
+    }
+}
