a841df608305dd28cb013fac78f96ff0520da7dd
shishnk <akorund@mail.ru>
2022-10-27 17:55:41 +0700

    Внес поправки

--- cg_2/Source/Wrappers/VertexBufferObject.cs
+++ cg_2/Source/Wrappers/VertexBufferObject.cs
@@ -10,10 +10,6 @@ public class VertexBufferObject<T> : IDisposable where T : unmanaged
 
     public void Bind() => GL.BindBuffer(BufferTarget.ArrayBuffer, Handle);
 
-    // TODO -> implement method for other type of vertex
-    public void BufferData(Vertex2N[] data) => GL.NamedBufferStorage(Handle, data[0].Size * data.Length, data,
-        BufferStorageFlags.MapWriteBit);
-
     public void BufferData(float[] data)
         => GL.BufferData(BufferTarget.ArrayBuffer, data.Length * Sizeof, data, TypeDraw);
 
