7d0a5ed1987143f9b6867e446a4b4863ce51a36f
lexangross51 <lexangross51@gmail.com>
2022-11-01 00:53:55 +0700

    Изменил int ptr на IntPtr ptr

--- cg_2/Source/Wrappers/VertexBufferWrapper.cs
+++ cg_2/Source/Wrappers/VertexBufferWrapper.cs
@@ -15,10 +15,10 @@ public class VertexBufferWrapper
         bool isNormalised,
         int size,
         int stride,
-        int ptr)
+        IntPtr ptr)
     {
         gl.BufferData(34962U, rawData, 35044U);
-        gl.VertexAttribPointer(attributeIndex, size, 5126U, isNormalised, stride, (IntPtr)ptr);
+        gl.VertexAttribPointer(attributeIndex, size, 5126U, isNormalised, stride, ptr);
         gl.EnableVertexAttribArray(attributeIndex);
     }
 
